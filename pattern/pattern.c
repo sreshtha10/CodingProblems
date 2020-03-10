f5170bae0e8d4318c1b30cdaba29bad8728f9ff2
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-03-10 18:40:10 +0530

    Update pattern.c

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -523,6 +523,25 @@ int main()
     
     printf("\n");
     
+
+    /* 35 */
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (j == i || j == 6-i)
+            {
+                printf("1");
+            }
+            else
+            {
+                printf("0");
+            }
+            
+        }
+        printf("\n");
+    }
+    
     
 
     

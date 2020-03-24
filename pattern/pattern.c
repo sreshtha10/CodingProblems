f3cd3a246eb155c0fdb1b5f63bc09aa03ef96e12
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-03-24 22:52:14 +0530

    Update pattern.c

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -593,5 +593,22 @@ int main()
         printf("\n");
     }
     
+    printf("\n");
+
+    /*40 */
+    for (int i = 1; i <=5; i++)
+    {
+        int count = 1;
+        for (int j = i; j <= 5; j++,count++)
+        {
+            printf("%d",j);
+        }
+        for (; count <= 5; count++)
+        {
+            printf("5");
+        }
+        printf("\n");
+    }
+    
     
 }

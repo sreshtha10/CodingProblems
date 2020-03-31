b10129b021d07682617d543a7f94477e13c005db
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-01 03:24:25 +0530

    Create Staircase.c

---
+++ hackerrank_problems/Staircase.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i =1 ; i<= n;i++)
+    {
+        for( int j = 1;j <= n-i;j++)
+        {
+            printf("%c",32);
+        }
+        for(int j =1; j<=i;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}

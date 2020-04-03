875c9a1cf74bd46a80b38823542e3dbab27acff8
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-03 23:38:52 +0530

    Update tall_men_next_door.py

--- Shakuntal Devi puzzle book/tall_men_next_door.py
+++ Shakuntal Devi puzzle book/tall_men_next_door.py
@@ -1,8 +1,8 @@
 import numpy as np
 #Let a,b,c,d be the height of the four brothers.
 #a+b+c+d = 74*4 = 296.
-#a-b = 2 , b-c = 2 , c-d = 6.
-a = np.array([[1,1,1,1],[1,-1,0,0],[0,1,-1,0],[0,0,1,-1]])
+#b-a = 2 , c-b = 2 , d-c = 6.
+a = np.array([[1,1,1,1],[-1,1,0,0],[0,-1,1,0],[0,0,-1,1]])
 b = np.array([[296],[2],[2],[6]])
 a = np.linalg.inv(a)
 c = np.dot(a,b)

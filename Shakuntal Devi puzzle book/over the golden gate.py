fee485842e02bde4d1914512b8d227d1e281d4a5
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-06 23:08:59 +0530

    Create over the golden gate.py

---
+++ Shakuntal Devi puzzle book/over the golden gate.py
@@ -0,0 +1,14 @@
+#starting speed = 40
+#speed while coming back  = 25
+#Let total distance be x
+#t1 and t2 be the time for first and second trips.
+#t1 = x/40 and t2  = x/20
+#average speed is 2x/((x/40)+(x/20))
+import numpy as np
+x = 1
+s1 = 40
+s2 = 25
+t1 = x/40
+t2 = x/25
+average_speed = (2*x)/(t1+t2)
+print(average_speed)

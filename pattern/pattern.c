ec90af5c755a99962eb721f26d71ec2978670bcb
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-09 23:00:42 +0530

    Update pattern.c

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -898,5 +898,78 @@ for(int i =1;i<=n-1;i++)
     printf("\n");
 }
 
+    /*56*/
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <=5-i; j++)
+        {
+            printf("%c",32);
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+    /* 57*/
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <=5-i; j++)
+        {
+            printf("%c",32);
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d",j);
+        }
+        for (int j = i+1;  j<= (2*i)-1 ; j++)
+        {
+            printf("%d",j);
+        }
+        
+        printf("\n");
+    }
+
+    printf("\n");
+    /* 58 */
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <=5-i; j++)
+        {
+            printf("%c",32);
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int j = i+1;  j<= (2*i)-1 ; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 4; i >= 1; i--)
+    {
+        for (int j = 1; j<= 5-i; j++)
+        {
+            printf("%c",32);
+        }
+        for (int j = i; j >= 1; j--)
+        {
+            printf("*");
+        }
+        for (int j = i-1; j>=1; j--)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+    
+
+
 
 }

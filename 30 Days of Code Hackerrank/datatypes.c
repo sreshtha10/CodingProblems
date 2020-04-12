6820ed2911ecffdfc49936c56fa9c71ef015236e
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-12 23:39:52 +0530

    Create datatypes.c

---
+++ 30 Days of Code Hackerrank/datatypes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+    int a;
+    double b;
+    char c[100];
+    scanf("%d",&a);
+    scanf("%lf",&b);
+    scanf("%*[\n] %[^\n]", c);
+    printf("%d\n",a+i);
+    printf("%.1lf\n",d+b);
+    printf("%s%s",s,c);
+    return 0;

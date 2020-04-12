ef6f467273187740a0745262dce9348e053143ed
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-12 23:45:28 +0530

    Update datatypes.c

--- 30 Days of Code Hackerrank/datatypes.c
+++ 30 Days of Code Hackerrank/datatypes.c
@@ -18,3 +18,26 @@ int main() {
     printf("%s%s",s,c);
     return 0;
 }
+
+/*2 method*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+   int a;
+    double b;
+    char c[100];
+    scanf("%d",&a);
+    scanf("%lf\n",&b);
+    fgets(c,sizeof(c),stdin);
+    printf("%d\n",a+i);
+    printf("%.1lf\n",d+b);
+    printf("%s",s);
+    puts(c);
+    return 0;
+}

48530c6c1ddf1aba6bac8276ff2bfe0d16154813
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-23 20:16:10 +0530

    Update electronic_shop.py

--- hackerrank_problems/electronic_shop.py
+++ hackerrank_problems/electronic_shop.py
@@ -1,14 +1,11 @@
-import itertools
 a = list(map(int,input().split()))
 budget = a[0]
 max_cost = -1
-l = []
 keyboards = list(map(int,input().split()))
 usbs = list(map(int,input().split()))
 for keyboard in keyboards:
     for usb in usbs:
-        l.append(keyboard+usb)
-for cost in l:
-    if cost > max_cost and cost <= budget:
-        max_cost = cost
+        cost = keyboard+usb
+        if cost > max_cost and cost <= budget:
+            max_cost = cost
 print(max_cost)

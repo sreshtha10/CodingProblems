5c047bb901c0897f2ae503224edcc596aa0f4989
Sreshtha Mehrotra <55186163+sreshthamehrotra00@users.noreply.github.com>
2020-04-24 01:12:30 +0530

    Update Beautiful_Days.py

--- hackerrank_problems/Beautiful_Days.py
+++ hackerrank_problems/Beautiful_Days.py
@@ -1,13 +1,6 @@
 arr = list(map(int,input().split()))
-def returnReverse(n):
-    reverse = 0
-    while n >0:
-        a = n%10
-        reverse = reverse*10 +a
-        n = n//10
-    return reverse
 count = 0
 for day in range(arr[0],arr[1]+1):
-    if (day-returnReverse(day))%arr[2] == 0:
+    if (day-int(str(day)[::-1]))%arr[2] == 0:
         count += 1
 print(count)

a76dbeb042743c754e1710ee8c60ca5b7ac1b87d
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-24 14:51:07 +0530

    To move the smallest element to the start of the LL

---
+++ others/Linked List/moveMin.java
@@ -0,0 +1,70 @@
+
+class LinkedList{
+    class Node{
+        int data;
+        Node next;
+    }
+        Node head;
+        public void insert(int data){
+            Node p = new Node();
+            p.data = data;
+            p.next = null;
+            if(head == null){
+                head = p;
+            }
+            else{
+                Node n = head;
+                while(n.next != null){
+                    n = n.next;
+                }
+                n.next = p;
+            }
+        }
+        
+        public void print(){
+            Node p = head;
+            if(head == null){
+                System.out.println("List is empty");
+            }
+            while(p!= null){
+                System.out.println(p.data+" ");
+                p = p.next;
+            }
+        }
+        public void insertStart(int data){
+            Node p = new Node();
+            p.data= data;
+            p.next = head;
+            head = p;
+            
+        }
+        public void moveMIN(){
+            Node p = head;
+            int tmp;
+            while(p != null){
+                if(head.data > p.data){
+                    tmp = head.data;
+                    head.data = p.data;
+                    p.data = tmp;
+                }
+                p = p.next;
+            }
+            return;
+        }
+
+        
+}
+
+
+public class Main{
+    public static void main(String args[]){
+        LinkedList linkedlist = new LinkedList();
+        linkedlist.insertStart(1);
+        linkedlist.insert(2);
+        linkedlist.insert(3);
+        linkedlist.insert(2);
+        linkedlist.insert(0);
+        linkedlist.moveMIN();
+        linkedlist.print();
+    }
+}

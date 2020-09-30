dc944bfb8860899f049cfc93500744ccee695724
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-30 22:53:27 +0530

    Split a SLL into two halves.

---
+++ others/Linked List/splitMiddle.java
@@ -0,0 +1,74 @@
+class LinkedList{
+    class Node{
+        int data;
+        Node next;
+    }
+        Node head;
+        public void insert(int data){
+            Node p = new Node();
+            p.data = data;
+            p.next = null;
+            if(head == null){
+                head = p;
+            }
+            else{
+                Node n = head;
+                while(n.next != null){
+                    n = n.next;
+                }
+                n.next = p;
+            }
+        }
+        
+        public void print(){
+            Node p = head;
+            if(head == null){
+                System.out.println("List is empty");
+            }
+            while(p!= null){
+                System.out.println(p.data+" ");
+                p = p.next;
+            }
+        }
+        public LinkedList splitMiddle(LinkedList l2){
+            Node p1 = this.head,p2 = this.head;
+            while(p2.next!=null && p2.next.next!=null){
+                p2 = p2.next.next;
+                p1 = p1.next;
+                
+            }
+            l2.head = p1.next;
+            Node tmp = l2.head.next;
+            while(tmp!=null){
+                Node q = new Node();
+                q.data = tmp.data;
+                q.next = tmp.next;
+                tmp =  tmp.next;
+            }
+            p1.next = null;
+            
+            return l2;
+        }
+        
+        
+    
+
+        
+}
+
+
+public class Main{
+    public static void main(String args[]){
+        LinkedList linkedlist = new LinkedList();
+        LinkedList l2 = new LinkedList();
+        linkedlist.insert(1);
+        linkedlist.insert(2);
+        linkedlist.insert(3);
+        linkedlist.insert(4);
+        linkedlist.insert(5);
+        linkedlist.insert(6);
+        l2 = linkedlist.splitMiddle(l2); 
+        l2.print();
+        linkedlist.print();
+    }
+}

42c6e4165e658965d503a03c5699bea719eff6a9
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-01 19:34:14 +0530

    Equal Stacks

--- hackerrank_problems/equalStacks.py
+++ hackerrank_problems/equalStacks.py
@@ -1,15 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
 def equalStacks(h1, h2, h3):
     s1,s2,s3 = map(sum,(h1,h2,h3))
     while h1 and h2 and h3:
-        m = min(s1,s2,s3)
-        while s1 > m:
-            s1 -= h1.pop(0)
-        while s2 > m:
-            s2 -= h2.pop(0)
-        while s3 > m:
-            s3 -= h3.pop(0)
-        if s1 == s1 == s3:
-            return s1
+        m = min(s1, s2, s3)
+        while s1 > m: s1 -= h1.pop(0)
+        while s2 > m: s2 -= h2.pop(0)
+        while s3 > m: s3 -= h3.pop(0)
+        if s1 == s2 == s3: return s1
     return 0
 
 

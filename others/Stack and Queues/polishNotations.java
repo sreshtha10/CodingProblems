302efcab62606e4c3b548f3992fa0aeb1997ac8d
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-03 23:58:09 +0530

    Polish Notations implementation using Stack

--- others/Stack and Queues/polishNotations.java
+++ others/Stack and Queues/polishNotations.java
@@ -1,89 +1,67 @@
 import java.util.Scanner;
-
-class Stack{
-	int top=-1;
-	int size;
-	char arr[];
-	public Stack(int size) {
-		this.size = size;
-		this.arr = new char[size];
-	}
-	public int isEmpty() { 
-		if(this.top == -1) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-	public int isFull() {
-		if(this.top == this.size -1) {
-			System.out.println("Stack Overflow");
-			return 1;
-		}
-		else {
-			return 0;
-		}
+import java.util.Stack;
+//Infix to Postfix conversion 
+public class Main{
+	public static void main(String argsp[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the infix expression");
+		String infix = scanner.nextLine();
+		String postfixString = infixtoPostfix(infix);
+		System.out.println("Postfix : "+postfixString);
+		scanner.close();
+		return;
 		
 	}
-	public char pop() {
-		if(this.isEmpty() == 1) {
-			System.out.println("Stack Overflow");
-			System.exit(1);
-		}
-		return arr[top--];
-	}
-	public void push(char x) {
-		if(this.isFull() == 1) {
-			return;
-		}
-		arr[++top] = x;
-		return;
-	}
-	public void printStack() {
-		if(isEmpty() == 1) {
-			return;
-		}
-		int p = this.top;
-		while(p != -1) {
-			System.out.println(arr[p--]);
-		}
-		return;
-	}
-}
-
-class PolishNotations{
-	String infix;
-	String postfix;
-	public PolishNotations(String infix) {
-		this.infix = infix;
-	}
-	private int priority(char symbol) {
+	public static int priority(char symbol) {
 		switch (symbol) {
-		case '^':
-			return 3;
 		case '+':
-		case '-':
-			return 1;
-		case '*':
-		case '/':
-		case '%':
-			return 2;
-		case '(':
-			return 0;
-		default:
-			return 0;
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%' :
+            return 2;
+        case '^':
+            return 3;
+        default: return 0;
 		}
 	}
-	
-	
-	
-}
-public class Main{
-	public static void main(String args[]) {
-		Stack s1 = new Stack(5);
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the infix");
-		String infix  = sc.nextLine();
+	public static String infixtoPostfix(String expression) {
+		String postfix = "";
+		Stack<Character> stack = new Stack<>();
+		for(int i=0;i<expression.length();i++) {
+			char c = expression.charAt(i);
+			/*check if char is operator then we will pop all the operators have higher or equal precedence than c a
+			push it postfix, and push the operator c in the stack.
+			*/
+			if(priority(c)>0) {
+				while(stack.isEmpty() == false && priority(stack.peek()) >= priority(c)) {
+					postfix += stack.pop();
+				}
+				stack.push(c);
+			}
+			//if c is right bracket then we will pop() and add that operator to postfix all the elements until we reach left bracket
+			else if(c == ')') {
+				char x = stack.pop();
+				while(x !='(') {
+					postfix += x;
+					x = stack.pop();
+				}
+			}
+			// if c is left bracket we will push it to the stack
+			else if(c == '(') {
+				stack.push(c);
+				
+			// if c is and operand we will add it to the postfix
+			}else {
+				postfix += c;
+			}
+		}
+		// when scanning is complete if any operator is still in the stack, we will pop() it and add to postfix
+		while(stack.isEmpty() != true) {
+			postfix += stack.pop();
+		}
+		return postfix;
 	}
+	
 }

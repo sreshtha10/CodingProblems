089db2d6fb077cbf5e3f84c3a7ff589d665ac3a8
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-10-15 03:04:46 +0530

    Reversing a number using Recursion

---
+++ others/Recursion/reverseNumber.py
@@ -0,0 +1,14 @@
+# reverse a number 
+num = int(input())
+s = 0
+def rev(num):
+    global s,x
+    if(num == 0):
+        return
+    y = num%10
+    s = s*10 +y
+    rev(num//10)
+    
+rev(num)
+print(s)
+    

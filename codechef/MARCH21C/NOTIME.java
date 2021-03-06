3ca24c92f09834e2fe05c25e27dd2def44efe5e1
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-03-07 00:51:23 +0530

    Create NOTIME.java

---
+++ codechef/MARCH21C/NOTIME.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*  // don't place package name! */
+
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws IOException
+	{
+	    try{
+	    	InputStreamReader r = new InputStreamReader(System.in);
+	    	BufferedReader reader = new BufferedReader(r);
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int H = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            if(x >H) {
+            	System.out.println("YES");
+            	return;
+            }
+            st = new StringTokenizer(reader.readLine());
+            for(int i=0;i<N;i++) {
+            	if(Integer.parseInt(st.nextToken())+x >= H) {
+            		System.out.println("YES");
+            		return;
+            	}
+            }
+            System.out.println("NO");
+            
+	    	
+            
+	    }
+	    catch(Exception e){
+	        return;
+	    }
+	}
+}

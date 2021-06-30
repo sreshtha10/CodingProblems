c2329ee157d415b667ef4a1e63c3a84ff48037ab
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-07-01 00:55:15 +0530

    Update Main.java

--- codechef/MARCH21C/NOTIME/Main.java
+++ codechef/MARCH21C/NOTIME/Main.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class Main
 {
 	public static void main (String[] args) throws IOException
 	{
